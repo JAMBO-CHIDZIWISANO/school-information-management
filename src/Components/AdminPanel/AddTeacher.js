e8c0f46e0d98f08c8bfa3aec748b35fc7c765010
JAMBO-CHIDZIWISANO <bed-com-26-17@unima.ac.mw>
2022-03-22 15:27:32 +0100

    Revert "Merge branch 'front-end' of https://github.com/JAMBO-CHIDZIWISANO/school-information-management into front-end"
    This reverts commit 3deb5f10209dd8f4d94948e4d08b447be2483d64, reversing
    changes made to ac51fe66980c7b8afd96f5ffe12d19c145eaa952.

--- src/Components/AdminPanel/AddTeacher.js
+++ src/Components/AdminPanel/AddTeacher.js
@@ -1,111 +1,89 @@
-import React, { useState, useRef } from "react";
-//import Form from "react-validation/build/form";
-//import Input from "react-validation/build/input";
-//import CheckButton from "react-validation/build/button";
-//import { isEmail } from "validator";
+import axios from "axios";
+import React, { useState } from "react";
+import userService from "../services/user.service";
 import AuthService from "../services/auth.service";
-// const required = (value) => {
-//   if (!value) {
-//     return (
-//       <div className="alert alert-danger" role="alert">
-//         This field is required!
-//       </div>
-//     );
-//   }
-// };
-// const validEmail = (value) => {
-//   if (!isEmail(value)) {
-//     return (
-//       <div className="alert alert-danger" role="alert">
-//         This is not a valid email.
-//       </div>
-//     );
-//   }
-// };
-const vusername = (value) => {
-  if (value.length < 3 || value.length > 20) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The username must be between 3 and 20 characters.
-      </div>
-    );
-  }
-};
-const vpassword = (value) => {
-  if (value.length < 6 || value.length > 40) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
-      </div>
-    );
-  }
-};
-const AddTeacher = (props) => {
-  const form = useRef();
-  //const checkBtn = useRef();
+
+const AddTeacher = () => {
+
+  const [id, setId] =useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   // form2- teacher details registration
+  const [teacherId, setTeacherId] = useState("");
   const [firstname, setFirstname] = useState("");
   const [middlename, setMiddlename] = useState("");
   const [lastname, setLastname] = useState("");
   const [gender, setGender] = useState("");
   const [qualification, setQualification] = useState("");
   const [joinDate, setJoinDate] = useState("");
-
+  const [userId, setUserId]= useState("");
   // setting out error messages
   const [successful, setSuccessful] = useState(false);
   const [message, setMessage] = useState("");
   // handling events
+
+  const onChangeId =(e)=>{
+    const id = e.target.value;
+    setId(id);
+  }
   const onChangeUsername = (e) => {
     const username = e.target.value;
     setUsername(username);
   };
-
   const onChangeEmail = (e) => {
     const email = e.target.value;
     setEmail(email);
   };
-
   const onChangePassword = (e) => {
     const password = e.target.value;
     setPassword(password);
   };
 
+
+  const onChangeTeacherId=(e)=>{
+    const teacherId = e.target.value;
+    setTeacherId(teacherId)
+  }
   const onChangeFirstname = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+    const firstname = e.target.value;
+    setFirstname(firstname);
   };
 
   const onChangeMiddlename = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+    const middlename = e.target.value;
+    setMiddlename(middlename);
   };
 
   const onChangeLastname = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+    const lastname = e.target.value;
+    setLastname(lastname);
   };
   const onChangeGender = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+    const gender = e.target.value;
+    setGender(gender);
   };
   const onChangeQualification = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+    const qualification = e.target.value;
+    setQualification(qualification);
   };
   const onChangeJoinDate = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+    const joinDate = e.target.value;
+    setJoinDate(joinDate);
   };
+  const onChangeUserId = (e) => {
+    const userId = e.target.value;
+    setUserId(userId);
+  };
+
+
 
   const handleRegister = (e) => {
     e.preventDefault();
     setMessage("");
     setSuccessful(false);
-    //form.current.validateAll();
-      AuthService.register(username, email, password).then(
+      AuthService.registerTeacher(id, username, email, password).then(
         (response) => {
           setMessage(response.data.message);
           setSuccessful(true);
@@ -124,58 +102,105 @@ const AddTeacher = (props) => {
     
   };
 
+  const handleAdd = (e)=>{
+    e.preventDefault();
+
+    setMessage("");
+    setSuccessful(false);
+      userService.teacherPersonalDetails(teacherId, 
+                  firstname, 
+                  middlename, 
+                  lastname, 
+                  gender,
+                  qualification, 
+                  joinDate, 
+                  userId).then(
+        (response) => {
+          setMessage(response.data.message);
+          setSuccessful(true);
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+          setMessage(resMessage);
+          setSuccessful(false);
+        }
+      );
+  }
+
   return (
     
-      <div className="container">
-        <div className="col-md-6">
+    <div className="container">
+      <div className="col-md-6">
         <h3 className="text-center" >Teacher Sign Up Form</h3><hr/>
+        
         <form onSubmit={handleRegister}>
           {!successful && (
-            <div>
-              <div className="form-group" >
-                <label htmlFor="username">Username</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="username"
-                  value={username}
-                  onChange={onChangeUsername}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <button className="btn btn-primary btn-block">Sign Up</button>
-              </div>
+            
+          <div>
+            <div className="form-group" >
+              <label htmlFor="id">User Id</label>
+              <input
+                type="text"
+                className="form-control"
+                name="id"
+                value={id}
+                onChange={onChangeId}
+                required
+              />
             </div>
+
+            <div className="form-group" >
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                value={username}
+                onChange={onChangeUsername}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={onChangeEmail}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChangePassword}
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <button className="btn btn-primary btn-block">Sign Up</button>
+            </div>
+
+          </div>
           )}
           {message && (
             <div className="form-group">
               <div
                 className={ successful ? "alert alert-success" : "alert alert-danger" }
-                role="alert"
-              >
+                role="alert">
                 {message}
               </div>
             </div>
@@ -183,10 +208,22 @@ const AddTeacher = (props) => {
         </form>
       </div>
       <br></br> <br></br>
+
       <div className="col-md-6">
-      <h3 className="text-center" >Additional Information</h3><hr/>
-      <form autoComplete="off" >
-          {!successful && (
+      <h3 className="text-center" >Personal Details</h3><hr/>
+      <form autoComplete="false" onSubmit={handleAdd} >
+          
+              <div className="form-group" >
+                <label htmlFor="teacherId">teacher id</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="teacherId"
+                  value={teacherId}
+                  onChange={onChangeTeacherId}
+                  required
+                />
+              </div>
             
               <div className="form-group" >
                 <label htmlFor="firstname">Firstname</label>
@@ -194,15 +231,17 @@ const AddTeacher = (props) => {
                   type="text"
                   className="form-control"
                   name="firstname"
-                  value={username}
+                  value={firstname}
                   onChange={onChangeFirstname}
                   required
                 />
-             
+              </div>
+
               <div className="form-group">
                 <label htmlFor="middlename">Middle Name</label>
                 <input
                   type="text"
+                  id="middlename"
                   className="form-control"
                   name="middlename"
                   value={middlename}
@@ -210,6 +249,7 @@ const AddTeacher = (props) => {
                   required
                 />
               </div>
+
               <div className="form-group">
                 <label htmlFor="lastname">Lastname</label>
                 <input
@@ -221,6 +261,7 @@ const AddTeacher = (props) => {
                   required
                 />
               </div>
+
               <div className="form-group">
                 <label htmlFor="gender">Gender</label>
                 <input
@@ -232,6 +273,7 @@ const AddTeacher = (props) => {
                   required
                 />
               </div>
+
               <div className="form-group">
                 <label htmlFor="qualification">Qualification</label>
                 <input
@@ -243,10 +285,11 @@ const AddTeacher = (props) => {
                   required
                 />
               </div>
+
               <div className="form-group">
                 <label htmlFor="joinDate">Join Date</label>
                 <input
-                  type="date"
+                  type="joinDate"
                   className="form-control"
                   name="joinDate"
                   value={joinDate}
@@ -254,21 +297,31 @@ const AddTeacher = (props) => {
                   required
                 />
               </div>
+
+              <div className="form-group">
+              <label htmlFor="userId">user id </label>
+              <input
+                type="userId"
+                className="form-control"
+                name="userId"
+                value={userId}
+                onChange={onChangeUserId}
+                required
+              />
+            </div>
               <div className="form-group">
                 <button className="btn btn-primary btn-block">Submit</button>
               </div>
-            </div>
-          )}
-          {message && (
+           {message && (
             <div className="form-group">
               <div
                 className={ successful ? "alert alert-success" : "alert alert-danger" }
-                role="alert"
-              >
+                role="alert">
                 {message}
               </div>
             </div>
           )}
+         
         </form>
         </div>
     </div>
