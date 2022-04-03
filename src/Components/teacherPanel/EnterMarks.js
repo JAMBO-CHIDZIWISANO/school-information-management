432429767393c22e0bcdac015d42471b564b43c2
JAMBO-CHIDZIWISANO <bed-com-26-17@unima.ac.mw>
2022-04-03 05:27:03 +0100

    full functional form for entering grades

--- src/Components/teacherPanel/EnterMarks.js
+++ src/Components/teacherPanel/EnterMarks.js
@@ -1,62 +1,111 @@
 import axios from 'axios';
 import React, { Component } from 'react'
+import Select from 'react-select'
+
 class EnterMarks extends Component {
 
-
   constructor(props){
+
     super(props);
-    this.onChangeMarkId = this.onChangeMarkId.bind(this);
-    this.onChangeStatus = this.onChangeStatus.bind(this);
+
+    this.onChangeType = this.onChangeType.bind(this);
     this.onChangeMarks = this.onChangeMarks.bind(this);
     this.onChangeStudentId = this.onChangeStudentId.bind(this)
-    this.onChangeSubjectCode = this.onChangeSubjectCode.bind(this)
-    this.onChangeClassId = this.onChangeClassId.bind(this)
-    this.onChangeTermId = this.onChangeTermId.bind(this)
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-      markId:"",
-      status: "",
+      type: "",
       marks: "",
       studentId:"",
+      
+      selectClass: [],
+      classId:"",
+      className:"",
+
+      selectSubject : [],
+      subjectName: "",
       subjectCode:"",
+
+      selectTerm:[],
       termId: "",
-      classId:""
+      termName:""
+     
+      
     }
   }
 
-  onChangeMarkId(e){
-    this.setState({markId: e.target.value})
+  async getSubjects(){
+    const res = await axios.get('http://localhost:4000/api/smis/getAllSubjects')
+    const data = res.data
+
+    const subjectOptions = data.map(d => ({
+      "value" : d.subjectCode,
+      "label" : d.subjectName
+
+    }))
+    this.setState({selectSubject: subjectOptions})
   }
-  onChangeStatus(e){
-    this.setState({ status: e.target.value})
+
+  async getClasses(){
+    const res = await axios.get('http://localhost:4000/api/smis/getAllClasses')
+    const data = res.data
+
+    const classesOptions = data.map(item => ({
+      "value" : item.classId,
+      "label" : item.className
+
+    }))
+    this.setState({selectClass: classesOptions})
   }
+
+  async getTerm(){
+    const res = await axios.get('http://localhost:4000/api/smis/getAllTerms')
+    const data = res.data
+
+    const termsOptions = data.map(item => ({
+      "value" : item.termId,
+      "label" : item.termName
+
+    }))
+    this.setState({selectTerm: termsOptions})
+  }
+
+  componentDidMount(){
+    this.getClasses()
+    this.getSubjects()
+    this.getTerm()
+  }
+
+ 
   onChangeMarks(e){
     this.setState({marks: e.target.value})
   }
   onChangeStudentId(e){
     this.setState({studentId: e.target.value})
   }
-  onChangeSubjectCode(e){
-    this.setState({subjectCode: e.target.value})
+  onChangeType(e){
+    this.setState({ type: e.target.value})
   }
-
-  onChangeTermId(e){
-    this.setState({termId: e.target.value})
-  }
-
   onChangeClassId(e){
-    this.setState({classId: e.target.value})
+    console.log(e)
+   this.setState({classId:e.value, className:e.label})
   }
-
-
+  onChangeTermId(e){
+    console.log(e)
+   this.setState({termId:e.value, termName:e.label})
+  }
+  onChangeSubjectCode(e){
+    console.log(e)
+   this.setState({subjectCode:e.value, subjectName:e.label})
+  }
+  
   onSubmit(e){
     e.preventDefault()
     const data = {
-        markId: this.state.markId,
-        status: this.state.status,
+       
         marks: this.state.marks,
         studentId: this.state.studentId,
+        type: this.state.type,
         subjectCode: this.state.subjectCode,
         classId: this.state.classId,
         termId: this.state.termId
@@ -69,17 +118,15 @@ class EnterMarks extends Component {
         console.log(error)
       });
     this.setState({
-        markId:"",
-        status: "",
+       
         marks: "",
         studentId:"",
-        subjectCode:"",
-        classId:""
+        type: this.state.type,
+        subjectCode:this.state.subjectCode,
+        termId:this.state.termId,
+        classId: this.state.classId
     })
   } 
-
- 
-  
 
   render(){
 
@@ -87,17 +134,6 @@ class EnterMarks extends Component {
     <div>
       <div className='px-5 wrapper mt-4 '>
         <form onSubmit={this.onSubmit} className=' mt-4'>
-
-          <div className='form-group'>
-            <label htmlFor='markId'>Mark Id</label>
-            <input   
-              name='markId'
-              value={this.state.markId}
-              onChange={this.onChangeMarkId}
-              className='form-control form-control-sm mt-2'
-              id='markId'
-              placeholder='markId'/>
-          </div>
 
           <div className='form-group'>
             <label htmlFor='marks'>Mark</label>
@@ -111,17 +147,6 @@ class EnterMarks extends Component {
           </div>
 
           <div className='form-group'>
-            <label htmlFor='status'>status</label>
-            <input
-              name='status'
-              value={this.state.status}
-              onChange={this.onChangeStatus}
-              className='form-control form-control-lg'
-              id='status'
-              placeholder='status'/>
-          </div>
-
-          <div className='form-group'>
             <label htmlFor='studentId'>student: </label>
             <input
               name='studentId'
@@ -132,58 +157,52 @@ class EnterMarks extends Component {
               placeholder="student "/>
           </div>
 
-          <div className='form-group'>
-            <label htmlFor='classId'>class: </label>
-            <input
-              name='classId'
-              value={this.state.classId}
-              onChange={this.onChangeClassId}
-              className='form-control form-control-sm'
-              id='classId'
-              placeholder="class "/>
+          <div className="form-group mt-3">
+            <label htmlFor="type">Type</label>
+            <select
+              value={this.state.type} 
+              onChange={this.onChangeType}
+              required>
+                <option></option>
+                <option>End-Of-Term</option>
+                <option>Assessment</option>
+            </select>
           </div>
 
-          <div className='form-group'>
-            <label htmlFor='subjectCode'>subject: </label>
-            <input
-              name='subjectCode'
-              type="text"
-              value={this.state.subjectCode}
-              onChange={this.onChangeSubjectCode}
-              className='form-control form-control-sm'
-              id='subjectCode'
-              placeholder="subject "/>
+          <div className='form-group mt-3'>
+            <Select 
+              placeholder="Select Class"
+              options={this.state.selectClass} 
+              onChange={this.onChangeClassId.bind(this)}
+            />
           </div>
 
-          <div className='form-group'>
-            <label htmlFor='subjectCode'>term: </label>
-            <input
-              name='termId'
-              type="text"
-              value={this.state.termId}
-              onChange={this.onChangeTermId}
-              className='form-control form-control-sm'
-              id='termId'
-              placeholder="termId "/>
+          <div className='form-group mt-3'>
+            <Select 
+              placeholder="Select Subject"
+              options={this.state.selectSubject} 
+              onChange={this.onChangeSubjectCode.bind(this)}
+            />
           </div>
 
-          <div>
+          <div className='form-group mt-3'>
+            <Select 
+              placeholder="Select Term"
+              options={this.state.selectTerm}
+               
+              onChange={this.onChangeTermId.bind(this)}
+            />
+          </div>
+
+          <div className='form-group mt-3'>
             <input 
               type="submit" 
               value="submit" 
-              className="btn btn-primary btn-block" />
-            </div>
-          </form>
+              className="btn btn-primary btn-block" 
+            />
+          </div>
+        </form>
       </div>
-
-      <div>
-
-      </div>
-
-      <div>
-
-      </div>
-
     </div>
   )}
 }
