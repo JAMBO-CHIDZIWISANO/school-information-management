461ce0fb13c2927c29e5dbba897f126ee9885072
Ernest Lasten <bed-com-05-17@cc.ac.mw>
2022-04-03 23:06:00 +0200

    upload feature front end and some retrieval functionality

--- src/Components/teacherPanel/UploadFile.js
+++ src/Components/teacherPanel/UploadFile.js
@@ -1,21 +1,39 @@
 import {useState} from 'react'
+import axios from "axios";
 
 const UploadFile = () => {
-
-  const [image, setImage] = useState("")
-
-  const handleChange = () => {
-    
-  }
+  // setting up state
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState("");
+ 
+  const saveFile = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+ 
+  const uploadFile = async (e) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("fileName", fileName);
+    try {
+      const res = await axios.post("http://localhost:4000/api/smis/upload", formData );
+      console.log(res);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
 
   return (
     <div>
+
       <div className='files'>
         <strong>Upload Time Table</strong>
-        <input type="file" value={image}  onChange={handleChange} />
+        <input type="file"  onChange={saveFile} />
+        <button onClick={uploadFile}>Upload</button>
       </div>
+
     </div>
-  )
+  );
 }
 
-export default UploadFile
+export default UploadFile;
